51ddce0c3cbbc5539ee01531e172fd5f22b974ad
TorrezMN <torrez.mn@gmail.com>
2023-04-12 10:04:19 -0400

    UPDATED frontend layout.

--- front/src/views/landing.js
+++ front/src/views/landing.js
@@ -7,14 +7,26 @@ function Landing(){
     <div className='landing_body'>
       <div className='landing_stats'>
         <div className='landing_stats_left'>
-          <p>left</p>
+          <p id='logo_n'>N</p>
+          <p id='logo_text'>Notes</p>
         </div>
         <div className='landing_stats_right'>
           <p>right</p>
+          <p>right</p>
+          <p>right</p>
+          <p>right</p>
+          <p>right</p>
         </div>
       </div>
-
-
+      <div className='landing_window'>
+        <div className='window_left'>
+ACA COMIENZA suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio.  Nunc porta vulputate tellus.  Nunc rutrum turpis sed pede.  Sed bibendum.  Aliquam posuere.  Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.  Pellentesque condimentum, magna ut suscipit hendrerit, ipsum augue ornare nulla, non luctus diam neque sit amet urna.  Curabitur vulputate vestibulum lorem.  Fusce sagittis, libero non molestie mollis, magna orci ultrices dolor, at vulputate neque nulla lacinia eros.  Sed id ligula quis est convallis tempor.  Curabitur lacinia pulvinar nibh.  Nam a sapien.
+        </div>
+        <div className='window_right'>
+Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio.  Nunc porta vulputate tellus.  Nunc rutrum turpis sed pede.  Sed bibendum.  Aliquam posuere.  Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.  Pellentesque condimentum, magna ut suscipit hendrerit, ipsum augue ornare nulla, non luctus diam neque sit amet urna.  Curabitur vulputate vestibulum lorem.  Fusce sagittis, libero non molestie mollis, magna orci ultrices dolor, at vulputate neque nulla lacinia eros.  Sed id ligula quis est convallis tempor.  Curabitur lacinia pulvinar nibh.  Nam a sapien.
+        </div>
+        
+      </div>
     </div>
   );
 
