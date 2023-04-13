21ec4bd103f5ef862b07b69c5da6c9a1077e9cdb
TorrezMN <torrez.mn@gmail.com>
2023-04-13 09:45:52 -0400

    FINISH adding base project urls.

--- back/routers/notes_router.py
+++ back/routers/notes_router.py
@@ -28,11 +28,41 @@ def notes_root():
     return {"notes": "root!"}
 
 
+####################
+#  NOTES
+####################
+
+
 @notes_router.get("/get_all_notes")
 def get_all_notes(db=Depends(db)):
     return {"notes": "get_all_notes!"}
 
 
+@notes_router.post("/filter_notes_by_content")
+def filter_notes_by_content(word, db=Depends(db)):
+    return {"notes": "filter_notes_by_content!"}
+
+
+@notes_router.post("/filter_notes_by_id")
+def filter_notes_by_id(id, db=Depends(db)):
+    return {"notes": "filter_notes_by_id!"}
+
+
+####################
+#  TAGS
+####################
+
+
 @notes_router.get("/get_all_tags")
 def get_all_tags(db=Depends(db)):
     return {"notes": "get_all_tags!"}
+
+
+@notes_router.post("/add_new_tag")
+def add_new_tag(word, db=Depends(db)):
+    return {"notes": "add_new_tag!"}
+
+
+@notes_router.post("/filter_tags")
+def filter_tags(word, db=Depends(db)):
+    return {"notes": "filter_tags!"}
